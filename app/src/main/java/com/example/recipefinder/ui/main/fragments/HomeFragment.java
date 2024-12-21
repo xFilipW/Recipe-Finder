0a3c7ab55322b72519fe3ad753501c784d58da1d
Michał Mokrzycki <michal@mokrzycki.eu>
2024-12-21 15:28:22 +0100

    feat: show no recipes messages when query size is 0

--- app/src/main/java/com/example/recipefinder/ui/main/fragments/HomeFragment.java
+++ app/src/main/java/com/example/recipefinder/ui/main/fragments/HomeFragment.java
@@ -1,10 +1,9 @@
 package com.example.recipefinder.ui.main.fragments;
 
-import static com.example.recipefinder.ui.main.fragments.HomeFragmentDirections.*;
+import static com.example.recipefinder.ui.main.fragments.HomeFragmentDirections.ActionMainHomeFragmentToRecipeDetailsFragment;
 
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +15,6 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.recipefinder.api.RepositoryUseCase;
 import com.example.recipefinder.database.RecipeTable;
@@ -26,7 +24,7 @@ import com.example.recipefinder.ui.main.adapters.CategoriesAdapter;
 import com.example.recipefinder.ui.main.adapters.RecipiesAdapter;
 import com.example.recipefinder.ui.main.fragments.itemDecorators.HorizontalSpaceItemDecoration;
 import com.example.recipefinder.ui.main.fragments.itemDecorators.VerticalSpaceItemDecoration;
-import com.example.recipefinder.ui.main.listeners.OnItemClickListenerEx;
+import com.example.recipefinder.ui.main.listeners.SimpleTextWatcher;
 import com.example.recipefinder.ui.main.repository.Repository;
 
 import java.util.Collections;
@@ -52,7 +50,7 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         navController = NavHostFragment.findNavController(this);
         initializeUI();
-        loadRandomRecipes(null, true);
+        loadRandomRecipes();
     }
 
     private void initializeUI() {
@@ -63,17 +61,7 @@ public class HomeFragment extends Fragment {
     }
 
     private void setupSearchView() {
-        binding.etSearch.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // left blank
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // left blank
-            }
-
+        binding.etSearch.addTextChangedListener(new SimpleTextWatcher() {
             @Override
             public void afterTextChanged(Editable phrase) {
                 queryRecipesByPhraseAndCategory(activeCategory, phrase.toString());
@@ -81,43 +69,55 @@ public class HomeFragment extends Fragment {
         });
     }
 
-    private void queryRecipesByPhraseAndCategory(String category, @NonNull String phrase) {
+    private void queryRecipesByPhraseAndCategory(@NonNull String category, @NonNull String phrase) {
         String trimmedPhrase = phrase.trim();
 
         repositoryUseCase.queryRecipesByPhraseAndCategory(
                 category,
                 trimmedPhrase,
                 data -> {
-                    recipiesAdapter.setData(data);
-
-                    String newPhrase;
-
-                    if (trimmedPhrase.isEmpty()) {
-                        newPhrase = (activeCategory == null || activeCategory.isEmpty())
-                                ? "200 random recipes each day"
-                                : "Recipes in " + activeCategory + ": " + data.size();
+                    if (data.isEmpty()) {
+                        displayNoRecipes();
                     } else {
-                        newPhrase = (activeCategory == null || activeCategory.isEmpty())
-                                ? "Recipes for \"" + trimmedPhrase + "\": " + data.size()
-                                : "Recipes for \"" + trimmedPhrase + "\" in " + activeCategory + ": " + data.size();
+                        displayRecipes(data);
                     }
+
+                    String newPhrase = handlePhraseMessage(trimmedPhrase, data);
 
                     String searchMessage = String.format(
                             Locale.getDefault(),
                             "%s",
                             newPhrase
                     );
+
                     binding.tvAmountOfRecipes.setText(searchMessage);
                 }
         );
+    }
 
+    @NonNull
+    private String handlePhraseMessage(@NonNull String trimmedPhrase, List<RecipeTable> data) {
+        String newPhrase;
+
+        if (activeCategory == null)
+            throw new IllegalArgumentException("activeCategory cannot be null");
+
+        if (trimmedPhrase.isEmpty()) {
+            newPhrase = activeCategory.isEmpty()
+                    ? "200 random recipes each day"
+                    : "Recipes in " + activeCategory + ": " + data.size();
+        } else {
+            newPhrase = activeCategory.isEmpty()
+                    ? "Recipes for \"" + trimmedPhrase + "\": " + data.size()
+                    : "Recipes for \"" + trimmedPhrase + "\" in " + activeCategory + ": " + data.size();
+        }
+        return newPhrase;
     }
 
     /**
      * @noinspection DataFlowIssue
      */
     private void setupCategoriesRecyclerView() {
-        binding.rvCategories.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
         CategoriesAdapter categoriesAdapter = new CategoriesAdapter(category -> {
             activeCategory = category.equals(RepositoryUseCase.ALL_RECIPES) ? "" : category;
             queryRecipesByPhraseAndCategory(activeCategory, binding.etSearch.getText().toString());
@@ -131,19 +131,19 @@ public class HomeFragment extends Fragment {
         repositoryUseCase = new RepositoryUseCase(requireContext());
     }
 
-    private void loadRandomRecipes(@Nullable String category, boolean initialRun) {
-        activeCategory = category;
+    private void loadRandomRecipes() {
+        activeCategory = "";
 
-        toggleLoadingState(true, initialRun, false);
+        toggleLoadingState(true);
 
         repositoryUseCase.getRecipes(new RandomRecipeResponseListener() {
             @Override
             public void onComplete(@NonNull List<RecipeTable> allRecipes) {
                 if (allRecipes.isEmpty()) {
-                    toggleLoadingState(false, initialRun, true);
+                    toggleLoadingState(false);
                     displayNoRecipes();
                 } else {
-                    toggleLoadingState(false, initialRun, false);
+                    toggleLoadingState(false);
                     displayRecipes(allRecipes);
                 }
             }
@@ -151,9 +151,9 @@ public class HomeFragment extends Fragment {
             @Override
             public void onError(String message) {
                 displayError(message);
-                toggleLoadingState(false, initialRun, false);
+                toggleLoadingState(false);
             }
-        }, category);
+        }, "");
     }
 
     private void displayRecipes(List<RecipeTable> recipes) {
@@ -164,13 +164,10 @@ public class HomeFragment extends Fragment {
 
     private void setupRecipesRecyclerView() {
         binding.rvRecipes.setLayoutManager(new GridLayoutManager(requireContext(), 2));
-        recipiesAdapter = new RecipiesAdapter(new OnItemClickListenerEx() {
-            @Override
-            public void onItemClick(long id) {
-                ActionMainHomeFragmentToRecipeDetailsFragment navDirections = HomeFragmentDirections.actionMainHomeFragmentToRecipeDetailsFragment(id);
-                //navDirections.setId(id);
-                navController.navigate(navDirections);
-            }
+        recipiesAdapter = new RecipiesAdapter(id -> {
+            ActionMainHomeFragmentToRecipeDetailsFragment navDirections = HomeFragmentDirections.actionMainHomeFragmentToRecipeDetailsFragment(id);
+            //navDirections.setId(id);
+            navController.navigate(navDirections);
         });
         binding.rvRecipes.setAdapter(recipiesAdapter);
         binding.rvRecipes.addItemDecoration(new HorizontalSpaceItemDecoration(HorizontalSpaceItemDecoration.SpanCount.TWO, 16, requireContext()));
@@ -187,29 +184,20 @@ public class HomeFragment extends Fragment {
         Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
     }
 
-    private void toggleLoadingState(boolean isLoading, boolean isInitial, boolean isEmpty) {
-        if (isInitial) {
-            int visibility = isLoading ? View.GONE : View.VISIBLE;
+    private void toggleLoadingState(boolean isLoading) {
+        int goneWhenLoading = isLoading ? View.GONE : View.VISIBLE;
+        int visibleWhenLoading = isLoading ? View.VISIBLE : View.GONE;
 
-            binding.rvCategories.setVisibility(visibility);
-            binding.tvRecipes.setVisibility(visibility);
-            binding.tvCategories.setVisibility(visibility);
-            binding.etSearch.setVisibility(visibility);
-            binding.tvAmountOfRecipes.setVisibility(visibility);
+        binding.rvCategories.setVisibility(goneWhenLoading);
+        binding.tvRecipes.setVisibility(goneWhenLoading);
+        binding.tvCategories.setVisibility(goneWhenLoading);
+        binding.etSearch.setVisibility(goneWhenLoading);
+        binding.tvAmountOfRecipes.setVisibility(goneWhenLoading);
 
-            binding.progressBar.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+        binding.progressBar.setVisibility(visibleWhenLoading);
 
-            if (isLoading) {
-                binding.tvNoRecipeFound.setVisibility(View.GONE);
-            }
-        } else { // specific category
-            if (isLoading) {
-                binding.progressBarRecipes.setVisibility(View.VISIBLE);
-                binding.rvRecipes.setVisibility(View.GONE);
-            } else {
-                binding.progressBarRecipes.setVisibility(View.GONE);
-                binding.rvRecipes.setVisibility(isEmpty ? View.GONE : View.VISIBLE);
-            }
+        if (isLoading) {
+            binding.tvNoRecipeFound.setVisibility(View.GONE);
         }
     }
 
